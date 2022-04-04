7534b0a6e5ead01941c5798f859c318a6e3d8299
dmddeleon <dmddeleon@gmail.com>
2022-04-04 23:56:14 +0800

    thesis

---
+++ config.py
@@ -0,0 +1,10 @@
+CATEGORIES = [
+    'allowed-direction', 'allowed-signs', 'cross-walk', 'probihited-direction',
+    'probihited-signs', 'speed-limit', 'stop-sign', 'traffic-sign', 'warning-sign'
+]
+DATA_DIR = 'dataset'
+SAVE_DIR = 'data'
+IMG_SIZE = 50
+INPUT_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+BATCH_SIZE = 36
+IMAGE_AUGMENT_MULTIPLEX = 4 
